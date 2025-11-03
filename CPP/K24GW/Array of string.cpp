08b3fc108c3dd887c2eae5da964434814ec238c2
enigmaAditya <adityakumar6002ad@gmail.com>
2025-11-03 13:28:08 +0530

    LinkedList Updated

--- CPP/K24GW/Array of string.cpp
+++ CPP/K24GW/Array of string.cpp
@@ -1,8 +1,94 @@
 #include <iostream>
 using namespace std;
+string substring(string str,int j, int size){
+    string res = "";
+    for(int i=j; i<=j+size; i++){
+        res+=str[i];
+    }
+    return res;
+}
+
+int find(string main, string word){
+    int n = main.length();
+    int m = word.length();
+    for(int i=0; i<=n-m; i++){
+        int j;
+        for(j=0; j<m; j++){
+            if(main[i+j]!=word[j]){
+                break;
+            }
+        }
+        if(j==m){
+            return i;
+        }
+    }
+    return -1;
+}
+
+string replace(string main, string old, string word){
+    string result = "";
+    int i = 0;
+
+    while(i < main.length()){
+        if(main.substr(i, old.length()) == old){
+            result += word;
+            i += old.length();
+        } else {
+            result += main[i];
+            i++;
+        }
+    }
+    return result;//output : Aditya is a awesome
+}
+
+string erase_substring(string main, string del){
+    string result = "";
+    int i = 0;
+    while(i < main.length()){
+        if(main.substr(i, del.length()) == del){
+            i += del.length();
+        } else {
+            result += main[i];
+            i++;
+        }
+    }
+    return result;//output : Aditya is a boy
+}
+
+//different string functions inbuilt
+//1. length() - returns the length of the string
+//2. find() - returns the index of the first occurrence of a substring
+//3. substr() - returns a substring from the string
+//4. replace() - replaces a substring with another substring
+//5. erase() - removes a substring from the string
+//6. append() - adds a substring at the end of the string
+//7. insert() - inserts a substring at a specified position in the string
+//8. clear() - removes all characters from the string
+//9. empty() - checks if the string is empty
+//10. at() - returns the character at a specified position in the string
+//11. c_str() - returns a pointer to a null-terminated character array
+//12. stoi() - converts a string to an integer
+//13. to_string() - converts an integer to a string
+//14. compare() - compares two strings
+//15. swap() - swaps the contents of two strings
+//16. pop_back() - removes the last character from the string
+//17. push_back() - adds a character at the end of the string
+//18. resize() - changes the size of the string
+//19. capacity() - returns the size of the allocated storage space for the string
+//20. reserve() - requests a change in the capacity of the string
+//21. shrink_to_fit() - reduces the capacity of the string to fit its size
+//22. front() - returns the first character of the string
+//23. back() - returns the last character of the string
+//24. find_first_of() - returns the index of the first occurrence of any character from a set
+//25. find_last_of() - returns the index of the last occurrence of any character from a set
+//26. find_first_not_of() - returns the index of the first occurrence of a character not in a set
+//27. find_last_not_of() - returns the index of the last occurrence of a character not in a set
+
+
+
 int main(){
     //string literal
-    char str[10]="Aditya" ;
+    char *str="Aditya" ;
     cout<<"input str: "<<str<<endl;
     //we have to make the string uppercase by using the ascii values:
     for(int i=0 ; str[i]!='\0' ; i++){
@@ -17,7 +103,46 @@ int main(){
             str[i]=str[i] ;
         }
     }
-    cout<<"Answer str: "<<str<<endl;
+    cout<<"Answer str: "<<str<<endl; //output : ADITYA
 
+
+    //split functionality
+    string s = "Aditya is learning C++" ;
+    string w[10] ;
+    int k=0 ;
+    int n = s.length();
+    string curr_word = "" ;
+    for(int i=0 ; i<n ; i++){
+        if(s[i]==' '){
+            w[k]=curr_word ;
+            curr_word="" ;
+            k++ ;
+        }
+        else{
+            curr_word+=s[i] ;
+        }
+    } 
+   w[k]=curr_word;
+   for(int i=0 ; i<=k ; i++){
+       cout<<w[i]<<endl; //output : Aditya \n is \n learning \n C++
+   }
+
+   //substring functionality
+    string str1 = "Aditya is a good boy" ;
+    string sub = substring(str1,8, 2); //output:
+    cout<<"Substring is: "<<sub<<endl;
+    return 0 ;
+
+    int f1 = str1.find("good");
+    cout<<"The index of good is: "<<f1<<endl;//output: The index of good is: 10
+    //find function call:
+    int f2 = find(str1, "good");
+    cout<<"The index of good is: "<<f2<<endl;//output: The index of good is: 10
+    //replaced function call:
+    string replaced_str = replace(str1, "good", "awesome");
+    cout<<"Replaced string is: "<<replaced_str<<endl;//output : Aditya is a awesome
+    //erase function call:
+    string erased_str = erase_substring(str1, "good ");
+    cout<<"Erased string is: "<<erased_str<<endl;//output : Aditya is a boy
     
-}
+   }
---
+++ CPP/K24GW/linkedList.cpp
@@ -0,0 +1,184 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+class LinkedList{
+    private:
+    Node* head;
+    public:
+    LinkedList(){
+        head = nullptr;
+    }
+
+    //take two parameter, node *head and int val
+    void insertAtEnd(Node* &head, int val){//explain: This function inserts a new node with the given value at the end of the linked list.  
+        Node* newNode = new Node(val);//explain: Create a new node with the provided value.
+        if(head == nullptr){//explain all: If the list is empty, set the new node as the head.
+            head = newNode;//explain: Update the head pointer to point to the new node.
+            return;//explain: Exit the function as the insertion is complete.
+        }
+        Node* temp = head;//explain: Start from the head of the list.
+        while(temp->next != nullptr){//explain: Traverse to the last node of the list.
+            temp = temp->next;//explain: Move to the next node.
+        }
+        temp->next = newNode;//explain: Link the last node to the new node, completing the insertion.
+    }//time complexity of above function is O(n)
+
+    
+    void insertAtEnd(Node* &head, int val){//better complexity version of insertAtEnd
+        //explain Node* &head: This parameter is a reference to a pointer to the head of the linked list. why both starn and  & ? because we want to modify the head pointer itself if the list is empty.
+        Node* newNode = new Node(val);
+        if(head == nullptr){
+            head = newNode;
+            return;
+        }
+        static Node* tail = head; //static keyword is used to retain the value of tail between function calls
+        tail->next = newNode;
+        tail = newNode;
+        //now newnode will point to null and tail will point to newnode
+        //how it will point to null? because when we create a new node, its next pointer is initialized to nullptr in the Node constructor
+    }//time complexity of above function is O(1)
+
+    void insertAtEnd(int val){//explain: This function inserts a new node with the given value at the end of the linked list.
+        Node* newNode = new Node(val);//explain: Create a new node with the provided value.
+        if(head == nullptr){//explain all: If the list is empty, set the new node as the head.
+            head = newNode;//explain: Update the head pointer to point to the new node.
+            return;//explain: Exit the function as the insertion is complete.
+        }
+        Node* temp = head;//explain: Start from the head of the list.
+        while(temp->next != nullptr){//explain: Traverse to the last node of the list.
+            temp = temp->next;//explain: Move to the next node.
+        }
+        temp->next = newNode;//explain: Link the last node to the new node, completing the insertion.
+    }
+
+    void insertAtBeginning(int val){//explain: This function inserts a new node with the given value at the beginning of the linked list.
+        Node* newNode = new Node(val);//explain: Create a new node with the provided value.
+        newNode->next = head;//explain: Point the new node's next to the current head of the list.
+        head = newNode;//explain: Update the head pointer to point to the new node, making it the first node in the list.
+    }
+
+    void insertAtPosition(int pos, int val){//explain: This function inserts a new node with the given value at the specified position in the linked list.
+        if(pos == 0){//explain: If the position is 0, we need to insert at the beginning.
+            insertAtBeginning(val);//explain: Call the function to insert at the beginning.
+            return;//explain: Exit the function as the insertion is complete.
+        }
+        Node* newNode = new Node(val);//explain: Create a new node with the provided value.
+        Node* temp = head;//explain: Start from the head of the list.
+        for(int i = 0; i < pos - 1 && temp != nullptr; i++){//explain: Traverse to the node just before the desired position.
+            temp = temp->next;//explain: Move to the next node.
+        }
+        if(temp == nullptr){//explain: If we reached the end of the list before reaching the desired position.
+            return;//explain: Exit the function as we cannot insert at this position.
+        }
+        newNode->next = temp->next;//explain: Point the new node's next to the current node's next.
+        temp->next = newNode;//explain: Link the current node to the new node, completing the insertion.
+    }
+
+    //any shorter version: Yes, we can combine the insertion logic into a single function
+    void insertAtPosition(int val,int pos,Node* &head){
+        Node* newNode = new Node(val);//explanation: Create a new node with the provided value.
+        if (pos == 0) {//explanation: If the position is 0, we need to insert at the beginning.
+            newNode->next = head;//explanation: Point the new node's next to the current head of the list.
+            head = newNode;//explanation: Update the head pointer to point to the new node, making it the first node in the list.
+            return;//explanation: Exit the function as the insertion is complete.
+        }
+        Node* temp = head;//explanation: Start from the head of the list.
+        for (int i = 0; i < pos - 1 && temp != nullptr; i++) {//explanation: Traverse to the node just before the desired position.
+            temp = temp->next;//explanation: Move to the next node.
+        }
+        if (temp == nullptr) {//explanation: If we reached the end of the list before reaching the desired position.
+            return;//explanation: Exit the function as we cannot insert at this position.
+        }
+        newNode->next = temp->next;//explanation: Point the new node's next to the current node's next.
+        temp->next = newNode;//explanation: Link the current node to the new node, completing the insertion.
+    }
+    //any other functions of linked list which is left foe us to implement?: Yes, there are several other common linked list operations that can be implemented, such as deleting a node, searching for a value, reversing the list, and finding the length of the list. Here's an implementation of the delete function:
+    void deleteValue(int val){//explain: This function deletes the first occurrence of a node with the specified value from the linked list.
+        if(head == nullptr){//explain : If the list is empty, there's nothing to delete.
+            return;//explain: Exit the function as no deletion can be performed.
+        }//explain: Check if the head node contains the value to be deleted.
+        if(head->data == val){//explain: If the head node contains the value to be deleted.
+            Node* temp = head;//explain: Store the current head in a temporary pointer.
+            head = head->next;//explain: Update the head to point to the next node.
+            delete temp;//explain: Free the memory of the old head node.
+            return;//explain: Exit the function as the deletion is complete.
+        }//explain: If the value is not at the head, we need to search for it in the rest of the list.
+        Node* curr = head;//explain: Start from the head of the list.
+        Node* prev = nullptr;//explain: Initialize a previous pointer to keep track of the node before the current one.
+        while(curr != nullptr && curr->data != val){//explain: Traverse the list until we find the value or reach the end.
+            prev = curr;//explain: Move the previous pointer to the current node.
+            curr = curr->next;//explain: Move to the next node.
+        }
+        if(curr == nullptr){//explain: If we reached the end of the list without finding the value.
+            return;//explain: Exit the function as there's nothing to delete.
+        }
+        prev->next = curr->next;//explain: Bypass the current node to remove it from the list.
+        delete curr;//explain: Free the memory of the node to be deleted.
+    }//time complexity of above function is O(n)
+
+    //function to search a value in linked list
+    bool search(int val){
+        //also return the position of the value if found
+        Node* temp = head;
+        int pos = 0;
+        while(temp != nullptr){
+            if(temp->data == val){
+                cout << "Value found at position: " << pos << endl;
+                return true;
+            }
+            temp = temp->next;
+            pos++;
+        }
+        return false;
+    }
+
+    //function to get length of linked list
+    int length(){
+        int len = 0;
+        Node* temp = head;
+        while(temp != nullptr){
+            len++;
+            temp = temp->next;
+        }
+        return len;
+    }
+
+    //function to display the linked list
+    void display(){
+        Node* temp = head;
+        while(temp != nullptr){
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr" << endl;
+    }
+    
+};
+
+class Solution {
+  public:
+    Node* arrayToList(vector<int>& arr) {
+        if (arr.empty()){
+            return nullptr;
+        }
+        Node* head = new Node(arr[0]);
+        Node* tail = head;
+
+        for (int i = 1; i<arr.size();i++) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+
+        return head;
+    }
+};
